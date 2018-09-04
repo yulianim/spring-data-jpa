0b5a613e86e9e4b593cfa5895b56ee433685be95
Yuliani <yuliani_m@hotmail.com>
2018-09-04 11:28:14 -0500

    Se agrego flash messenger para mensjes personalizados al crear, editar, guardar o eliminar, se agrego paginador desde cero con la clase PagingAndSortingRepository

--- src/main/java/com/milysoft/controller/ClienteController.java
+++ src/main/java/com/milysoft/controller/ClienteController.java
@@ -5,14 +5,19 @@ import java.util.Map;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.milysoft.model.Cliente;
 import com.milysoft.service.IClienteService;
@@ -24,9 +29,11 @@ public class ClienteController{
 	private IClienteService clienteService;
 	
 	@RequestMapping(value="/listar", method=RequestMethod.GET)
-	public String listar(Model model) {
+	public String listar(@RequestParam(name ="page", defaultValue="0") int page, Model model) {
+		Pageable pageRequest =new PageRequest(page, 5);
+		Page<Cliente> clientes=clienteService.findAll(pageRequest);
 		model.addAttribute("titulo","Listado de clientes");
-		model.addAttribute("cliente",clienteService.findAll());
+		model.addAttribute("cliente",clientes);
 		return "listar";
 	}
 	@RequestMapping(value="/form")
@@ -37,21 +44,28 @@ public class ClienteController{
 		return "form";
 	}
 	@RequestMapping(value="/form", method=RequestMethod.POST)
-	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, SessionStatus status) {
+	public String guardar(@Valid Cliente cliente, BindingResult result, RedirectAttributes flash, Model model, SessionStatus status) {
 		if(result.hasErrors()) {
 			model.addAttribute("titulo", "Formulario de clientes");
 			return "form";
 		}
+		String mensajeFlash=(cliente.getId()!=null)?  "Cliente editado con exito" :  "Cliente creado con exito";
 		clienteService.save(cliente);
 		status.setComplete();
+		flash.addFlashAttribute("success", mensajeFlash);
 		return "redirect:listar";		
 	}
 	@RequestMapping(value="/form/{id}")
-	public String editar(@PathVariable(value="id")  Long id, Map<String, Object> model) {
+	public String editar(@PathVariable(value="id")  Long id, Map<String, Object> model, RedirectAttributes flash) {
 		Cliente cliente=null;
 		if(id>0) {
 			cliente=clienteService.findOne(id);
+			if(cliente==null) {
+				flash.addFlashAttribute("error", "El ID del cliente no existe en la base de datos");
+				return "redirect:/listar";
+			}
 		}else {
+			flash.addFlashAttribute("error", "El ID del cliente es cero");
 			return "redirect:/listar";
 		}
 		
@@ -60,9 +74,11 @@ public class ClienteController{
 		return "form";
 	}
 	 @RequestMapping(value="/eliminar/{id}")
-	 public String eliminar(@PathVariable(value="id") Long id) {
+	 public String eliminar(@PathVariable(value="id") Long id, RedirectAttributes flash) {
 		 if(id>0){
 			 clienteService.delete(id);
+			 flash.addFlashAttribute("success", "Cliente eliminado con exito");
+				
 		 }
 		 return "redirect:/listar";
 		 
--- src/main/java/com/milysoft/service/IClienteService.java
+++ src/main/java/com/milysoft/service/IClienteService.java
@@ -2,10 +2,14 @@ package com.milysoft.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.milysoft.model.Cliente;
 
 	public interface IClienteService {
 	public List<Cliente> findAll();
+	public Page<Cliente> findAll(Pageable pageable);
 	public void save(Cliente cliente);
 	public Cliente findOne(Long id);
 	public void delete(Long id);
