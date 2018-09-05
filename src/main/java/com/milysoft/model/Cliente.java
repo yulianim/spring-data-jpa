68593c6c45e69987c137710708a3e582b2245130
Yuliani <yuliani_m@hotmail.com>
2018-09-05 17:23:01 -0500

    ManytoOne Bidireccional clases Entity Factura y cliente

--- src/main/java/com/milysoft/model/Cliente.java
+++ src/main/java/com/milysoft/model/Cliente.java
@@ -1,13 +1,18 @@
 package com.milysoft.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -43,6 +48,12 @@ public class Cliente implements Serializable{
 	@DateTimeFormat(pattern="yyyy-MM-dd")
 	private Date createAt;
 	
+	@OneToMany(mappedBy="cliente",fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	private List<Factura> facturas;
+	
+	public Cliente() {
+		facturas=new ArrayList<Factura>();
+	}
 	private String foto;
 	
 	public String getFoto() {
@@ -81,5 +92,13 @@ public class Cliente implements Serializable{
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
 	}
-	
+	public List<Factura> getFacturas() {
+		return facturas;
+	}
+	public void setFacturas(List<Factura> facturas) {
+		this.facturas = facturas;
+	}
+	public void addFactura(Factura factura) {
+		facturas.add(factura);
+	}
 }
