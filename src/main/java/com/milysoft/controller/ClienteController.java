6de8fd8daa30bbfb35b8a2b6525675f9daa8e7b1
Yuliani <yuliani_m@hotmail.com>
2018-09-05 15:46:03 -0500

    Se agrego la clase UploadFileService, se paso parte del codigo del controler a esta nueva clase para ordenar el codigo

--- src/main/java/com/milysoft/controller/ClienteController.java
+++ src/main/java/com/milysoft/controller/ClienteController.java
@@ -1,21 +1,12 @@
 package com.milysoft.controller;
 
-import java.io.File;
+
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Map;
-import java.util.UUID;
-
 import javax.validation.Valid;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -36,6 +27,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.milysoft.model.Cliente;
 import com.milysoft.service.IClienteService;
+import com.milysoft.service.IUploadFileService;
 import com.milysoft.util.paginator.PageRender;
 
 @Controller
@@ -43,18 +35,15 @@ import com.milysoft.util.paginator.PageRender;
 public class ClienteController{
 	@Autowired
 	private IClienteService clienteService;
-	private final Logger log=LoggerFactory.getLogger(getClass());
-	private final static String UPLOADS_FOLDER="uploads";
+	
+	@Autowired
+	private IUploadFileService uploadFileService;
+
 	@GetMapping(value="/uploads/{filename:.+}")
 	public ResponseEntity<Resource> verFoto(@PathVariable String filename){
-		Path pathFoto=Paths.get(UPLOADS_FOLDER).resolve(filename).toAbsolutePath();
-		log.info("pathFoto: "+pathFoto);
 		Resource recurso=null;
 		try {
-			recurso= new UrlResource(pathFoto.toUri());
-			if(!recurso.exists()&& !recurso.isReadable()) {
-				throw new RuntimeException("Error: no se puede cargar la imagen"+ pathFoto.toString());
-			}
+			recurso = uploadFileService.load(filename);
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -89,38 +78,29 @@ public class ClienteController{
 	public String crear(Map<String, Object> model) {
 		Cliente cliente=new Cliente();
 		model.put("cliente", cliente);
-		model.put("titulo", "Formulario de clientes");
+		model.put("titulo", "Crear cliente");
 		return "form";
 	}
 	@RequestMapping(value="/form", method=RequestMethod.POST)
-	public String guardar(@Valid Cliente cliente, BindingResult result, RedirectAttributes flash, Model model, @RequestParam("file") MultipartFile foto, SessionStatus status) {
+	public String guardar(@Valid Cliente cliente, BindingResult result, RedirectAttributes flash, Model model, @RequestParam("file") MultipartFile foto, SessionStatus status) 
+	{
 		if(result.hasErrors()) {
 			model.addAttribute("titulo", "Formulario de clientes");
 			return "form";
 		}
 		if(!foto.isEmpty()) {
 			if(cliente.getId()!=null && cliente.getId()>0 && cliente.getFoto().length()>0){
-				Path rootPath=Paths.get(UPLOADS_FOLDER).resolve(cliente.getFoto()).toAbsolutePath();
-				 File archivo=rootPath.toFile();
-				 
-				 if(archivo.exists()&&archivo.canRead()) {
-					 archivo.delete();
-				 }
+				uploadFileService.delete(cliente.getFoto());
 			}
-			String uniqueFilename=UUID.randomUUID().toString()+"_"+foto.getOriginalFilename();
-			Path rootPath=Paths.get(UPLOADS_FOLDER).resolve(uniqueFilename);
-			Path rootAbsolutPath=rootPath.toAbsolutePath();
-			log.info("rootPath: "+rootPath);
-			log.info("rootAbsolutPath: "+rootAbsolutPath);
+			String uniqueFilename=null;
 			try {
-				
-				Files.copy(foto.getInputStream(), rootAbsolutPath);
-				flash.addFlashAttribute("info","Archivo cargado correctamente'"+ uniqueFilename+"'");
-				cliente.setFoto(uniqueFilename);
+				uniqueFilename = uploadFileService.copy(foto);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			flash.addFlashAttribute("info","Archivo cargado correctamente'"+ uniqueFilename+"'");
+			cliente.setFoto(uniqueFilename);
 		}
 		String mensajeFlash=(cliente.getId()!=null)?  "Cliente editado con exito" :  "Cliente creado con exito";
 		clienteService.save(cliente);
@@ -152,14 +132,9 @@ public class ClienteController{
 			 Cliente cliente=clienteService.findOne(id);
 			 clienteService.delete(id);
 			 flash.addFlashAttribute("success", "Cliente eliminado con exito");
-			 Path rootPath=Paths.get(UPLOADS_FOLDER).resolve(cliente.getFoto()).toAbsolutePath();
-			 File archivo=rootPath.toFile();
-			 
-			 if(archivo.exists()&&archivo.canRead()) {
-				 if(archivo.delete()) {
-					 flash.addFlashAttribute("info", "Foto"+cliente.getFoto()+ "Eliminada con exito!");
-				 }
-			 }
+			if(uploadFileService.delete(cliente.getFoto())) {
+				flash.addFlashAttribute("info", "Foto" + cliente.getFoto()+ "Eliminado con exito!");
+			}
 		 }
 		 return "redirect:/listar";
 		 
---
+++ src/main/java/com/milysoft/service/IUploadFileService.java
@@ -0,0 +1,14 @@
+package com.milysoft.service;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface IUploadFileService {
+	public Resource load(String filename) throws MalformedURLException;
+	public String copy(MultipartFile file) throws IOException;
+	public boolean delete(String filename);
+	
+}
