9dd8bb0817e9c98387d501c45c85a26a19f53f18
Yuliani <yuliani_m@hotmail.com>
2018-09-11 12:49:10 -0500

    Creando la vista formulario de factura

--- src/main/java/com/milysoft/controller/FacturaController.java
+++ src/main/java/com/milysoft/controller/FacturaController.java
@@ -1,12 +1,39 @@
 package com.milysoft.controller;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.milysoft.model.Cliente;
+import com.milysoft.model.Factura;
+import com.milysoft.service.IClienteService;
 
 @Controller
 @RequestMapping("/factura")
+@SessionAttributes("factura")
 public class FacturaController {
-	public String crear() {
+	@Autowired
+	private IClienteService clienteService;
+	@GetMapping("/form/{clienteId}")
+	public String crear(@PathVariable(value="clienteId") Long clienteId, Map<String, Object> model, RedirectAttributes flash) {
+		
+		Cliente cliente=clienteService.findOne(clienteId);
+		if(cliente==null) {
+			flash.addFlashAttribute("Error", "El cliente no existe");
+			return "redirect:/listar";
+		}
+		Factura factura =new Factura();
+		factura.setCliente(cliente);
+		
+		model.put("factura", factura);
+		model.put("titulo", "Crear factura");
+		
 		return "factura/form";		
 	}
 
