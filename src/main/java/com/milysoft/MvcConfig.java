b14ac9f229a2d40339fd2bfb074f6bee27790578
Yuliani <yuliani_m@hotmail.com>
2018-09-23 23:12:22 -0500

    Implementando la clase SpringSecurityConfig para registrar usuarios, se cambio de version de 2.0.0 RC1 a 2.0.0. RELEASE, se agrego metodo addViewControllers en MvcConfig

--- src/main/java/com/milysoft/MvcConfig.java
+++ src/main/java/com/milysoft/MvcConfig.java
@@ -1,6 +1,7 @@
 package com.milysoft;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -15,6 +16,8 @@ public class MvcConfig implements WebMvcConfigurer{
 		log.info(resourcePath);
 		registry.addResourceHandler("/uploads/**").addResourceLocations(resourcePath);
 	}**/
-
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/error_403").setViewName("error_403");
+	}
 		
 }
