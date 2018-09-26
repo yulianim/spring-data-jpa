130c390f6bfeada00af86d0c6eff667504617de7
Yuliani <yuliani_m@hotmail.com>
2018-09-26 13:57:46 -0500

    Authenticacion JDBC usando base de datos: Bcrypt Password encoder

--- src/main/java/com/milysoft/MvcConfig.java
+++ src/main/java/com/milysoft/MvcConfig.java
@@ -1,6 +1,8 @@
 package com.milysoft;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,5 +21,8 @@ public class MvcConfig implements WebMvcConfigurer{
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/error_403").setViewName("error_403");
 	}
-		
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
