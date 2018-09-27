d9e9a00269763a9200cbc3935890a4a50d5396c9
Yuliani <yuliani_m@hotmail.com>
2018-09-26 21:23:11 -0500

    Configurando JDBC authentication

--- src/main/java/com/milysoft/SpringSecurityConfig.java
+++ src/main/java/com/milysoft/SpringSecurityConfig.java
@@ -1,15 +1,14 @@
 package com.milysoft;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.milysoft.auth.handler.LoginSuccessHandler;
 @EnableGlobalMethodSecurity(securedEnabled=true, prePostEnabled=true)
@@ -17,6 +16,11 @@ import com.milysoft.auth.handler.LoginSuccessHandler;
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	private LoginSuccessHandler successHandler;
+	@Autowired 
+	private DataSource dataSource;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/", "/css/**","/js/**", "/image/**", "/listar").permitAll().
@@ -35,12 +39,17 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	public void configurerGlobal(AuthenticationManagerBuilder build)throws Exception
 	{
-		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		build.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder)
+		.usersByUsernameQuery("select username, password, enabled from users where username=?")
+		.authoritiesByUsernameQuery("select u.username, a.authority from authorities a inner join users u on(a.user_id=u.id) where u.username=?");
+		
+		/*PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 		UserBuilder users = User.builder().passwordEncoder(encoder::encode);
 		
 		build.inMemoryAuthentication()
 		.withUser(users.username("admin").password("12345").roles("ADMIN", "USER"))
-		.withUser(users.username("yuliani").password("12345").roles("USER"));
+		.withUser(users.username("yuliani").password("12345").roles("USER"));**/
+		
 	}
 
 }
