60417b7d98c931428ceefd4df96aea8e35d03247
Yuliani <yuliani_m@hotmail.com>
2018-09-30 17:03:24 -0500

    Creando el controladore LocalController

--- src/main/java/com/milysoft/SpringSecurityConfig.java
+++ src/main/java/com/milysoft/SpringSecurityConfig.java
@@ -23,7 +23,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 	private JpaUserDetailService userDetailService;
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/", "/css/**","/js/**", "/image/**", "/listar").permitAll().
+		http.authorizeRequests().antMatchers("/", "/css/**","/js/**", "/image/**", "/listar", "/locale").permitAll().
 		//antMatchers("/ver/**").hasAnyRole("USER").
 		//antMatchers("/uploads/**").hasAnyRole("USER").
 		//antMatchers("/form/**").hasAnyRole("ADMIN").
